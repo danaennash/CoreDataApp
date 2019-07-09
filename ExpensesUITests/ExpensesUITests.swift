04ea96f0cb5c5b3660013338b1743acb95c9b123
danaennash <36461588+danaennash@users.noreply.github.com>
2019-07-08 20:00:11 -0500

    Add files via upload

---
+++ ExpensesUITests/ExpensesUITests.swift
@@ -0,0 +1,32 @@
+//
+//  ExpensesUITests.swift
+//  ExpensesUITests
+//
+//  Created by Danae N Nash on 7/8/19.
+//  Copyright © 2019 Danae N Nash. All rights reserved.
+//
+
+import XCTest
+
+class ExpensesUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+    
+        continueAfterFailure = false
+  
+        XCUIApplication().launch()
+
+  
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+ 
+    }
+    
+}
